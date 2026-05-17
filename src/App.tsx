40c6a1558ae8fde00816d1ad02deefcfb942ec1f
AJRoxas <ajroxdev@gmail.com>
2026-05-16 23:12:20 -0400

    Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,17 @@ import WelcomeBox from './components/WelcomeBox';
 
 const App = () => {
   return (
-    <main>
-      <div className='h-dvh min-h-87 flex flex-col justify-center items-center gap-4'>
-        <WelcomeBox />
-        <TitleGenerator />
-      </div>
-    </main>
+    <>
+      <main>
+        <div className="h-hdh min-h-87 flex flex-col justify-center items-center gap-4">
+          <WelcomeBox />
+          <TitleGenerator />
+        </div>
+      </main>
+      <footer className="fixed bottom-0 left-0 right-0 h-16 flex justify-center items-center w-full bg-off-black border-t-2">
+        &copy; 2026-Present Aliel Jacob Roxas :)
+      </footer>
+    </>
   );
 };
 
