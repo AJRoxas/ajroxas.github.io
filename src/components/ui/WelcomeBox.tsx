d6bde956c6498704d077024737d46333a1fab5bc
AJRoxas <ajroxdev@gmail.com>
2026-05-17 22:54:36 -0400

    Added bg to welcome box

--- src/components/ui/WelcomeBox.tsx
+++ src/components/ui/WelcomeBox.tsx
@@ -1,12 +1,12 @@
 const WelcomeBox = () => {
   return (
-    <>
-      <div className="h-75 w-75 font-sync font-bold text-off-white text-center text-5xl border-8 border-off-white flex flex-col justify-center gap-6 items-center">
+    <div className="h-75 w-75 bg-(image:--background-image) bg-cover flex justify-center items-center">
+      <div className="h-70 w-70 font-sync font-bold text-off-white text-center text-5xl bg-off-black/50 border-8 border-off-white flex flex-col justify-center gap-6 items-center">
         <p>Aliel</p>
         <p>Jacob</p>
         <p>Roxas</p>
       </div>
-    </>
+    </div>
   );
 };
 
