c366407a0a99785425c9af167e28f63c72006fbe
AJRoxas <ajroxdev@gmail.com>
2026-05-17 20:58:15 -0400

    Add skeleton sections to view

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,27 @@ import WelcomeBox from './components/WelcomeBox';
 const App = () => {
   return (
     <>
-      <main>
+      <main className="mb-16">
         <div className="h-hdh min-h-87 flex flex-col justify-center items-center gap-4">
           <WelcomeBox />
           <TitleGenerator />
+        </div>
+        <div className="flex flex-col justify-center items-center gap-4">
+          <div className="container">
+            <h2>// About</h2>
+          </div>
+          <div className="container">
+            <h2>// Work</h2>
+          </div>
+          <div className="container">
+            <h2>// Projects</h2>
+          </div>
+          <div className="container">
+            <h2>// Education</h2>
+          </div>
+          <div className="container">
+            <h2>// Contact</h2>
+          </div>
         </div>
       </main>
       <footer className="fixed bottom-0 left-0 right-0 h-16 flex justify-center items-center w-full bg-off-black border-t-2">
--- src/components/WelcomeBox.tsx
+++ src/components/WelcomeBox.tsx
@@ -1,10 +1,10 @@
 const WelcomeBox = () => {
   return (
     <>
-      <div className="h-75 w-75 font-sync font-bold text-off-white text-center text-6xl border-8 border-off-white flex flex-col justify-evenly items-center">
-        <p>ALIEL</p>
-        <p>JACOB</p>
-        <p>ROXAS</p>
+      <div className="h-75 w-75 font-sync font-bold text-off-white text-center text-5xl border-8 border-off-white flex flex-col justify-center gap-6 items-center">
+        <p>Aliel</p>
+        <p>Jacob</p>
+        <p>Roxas</p>
       </div>
     </>
   );
