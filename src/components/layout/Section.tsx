0d94bc2143088d127ce10349d60563d91589ceab
AJRoxas <ajroxdev@gmail.com>
2026-05-24 22:28:19 -0400

    Fixed sections widths

--- src/components/layout/Section.tsx
+++ src/components/layout/Section.tsx
@@ -7,9 +7,7 @@ interface SectionProps {
 const Section = ({ id, title, children }: SectionProps) => {
   return (
     <section id={id} className="section-container">
-      <div className="flex justify-center">
-        <h2 className="max-w-s1080">{title}</h2>
-      </div>
+      <h2 className="max-w-s1260">{title}</h2>
       <div className="flex flex-col items-center gap-8">{children}</div>
     </section>
   );
