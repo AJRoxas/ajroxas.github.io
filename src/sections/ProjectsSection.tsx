51b8dc84169d2c97b9af4da20eb3b6d5f0d8407a
AJRoxas <ajroxdev@gmail.com>
2026-05-26 21:35:06 -0400

    Added personal portfolio project

--- src/sections/ProjectsSection.tsx
+++ src/sections/ProjectsSection.tsx
@@ -5,8 +5,18 @@ const ProjectsSection = () => {
   return (
     <Section id="projects" title="// Projects">
       <ActivityContent
+        title="Personal Portfolio"
+        subtitle="Personal Project - This Website!"
+        duration="February 2026 - Present"
+        responsibilities={[
+          'Designed and created a personal portfolio in the React framework to showcase qualifications and skills',
+          'Implemented and automated deployment workflows using Docker and GitHub Pages',
+        ]}
+      />
+      <ActivityContent
+        reverse={true}
         title="Fitbook"
-        subtitle="Introduction to Software Engineering, University of Toronto"
+        subtitle="University Project"
         duration="May 2023 - August 2023"
         responsibilities={[
           'Built a fitness social media app with workout and progress tracking using React Native for iOS and Android.',
