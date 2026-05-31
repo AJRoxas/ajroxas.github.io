e61b15455f6e5cf50a7b9acc7a787bb503a266fa
AJRoxas <ajroxdev@gmail.com>
2026-05-30 21:13:51 -0400

    Update SkillBox formatting

--- src/components/ui/SkillBox.tsx
+++ src/components/ui/SkillBox.tsx
@@ -1,8 +1,8 @@
 const SkillBox = ({ image, skill }: { image: string; skill: string }) => {
   return (
-    <div className="max-w-30 flex justify-center items-center gap-2"> 
+    <div className="min-h-12 px-2 flex justify-center items-center gap-2"> 
       <img src={image} alt={`${skill} logo`} className="w-8 h-8" />
-      <span className="text-off-black text-sm font-bold">{skill}</span>
+      <span className="text-off-black text-center text-sm font-bold">{skill}</span>
     </div>
   );
 };  
